d4b387def4cfd4240b42b2d9e84736acc8f383cb
Jennifer Kennedy <jenken131@gmail.com>
2021-06-21 16:48:27 -0700

    added mikro-orm and created db

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello world");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    entities: [],
+    dbName: "bulletjournal",
+    type: "postgresql",
+    debug: !__prod__,
+  });
+};
+
+main();
