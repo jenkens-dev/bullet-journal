afb615710c79d1671c9ec2b8fecf294ce6cd5997
Jennifer Kennedy <jenken131@gmail.com>
2021-06-23 19:36:40 -0700

    set up express server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,18 @@
 import { MikroORM } from "@mikro-orm/core";
 import mikroOrmConfig from "./mikro-orm.config";
+import express from "express";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
   orm.getMigrator().up();
+
+  const app = express();
+  app.get("/", (_, res) => {
+    res.send("hello");
+  });
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
 };
 
 main().catch((err) => {
