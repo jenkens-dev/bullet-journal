577baf4323d3f9ed04da63449c8668d6f39560a8
Jennifer Kennedy <jenken131@gmail.com>
2021-06-24 19:29:19 -0700

    added createTodo resolver

--- server/src/resolvers/ToDo.ts
+++ server/src/resolvers/ToDo.ts
@@ -1,6 +1,6 @@
 import { ToDo } from "../entities/ToDo";
 import { MyContext } from "src/types";
-import { Query, Resolver, Ctx, Arg, Int } from "type-graphql";
+import { Query, Resolver, Ctx, Arg, Mutation } from "type-graphql";
 
 @Resolver()
 export class ToDoResolver {
@@ -10,10 +10,17 @@ export class ToDoResolver {
   }
 
   @Query(() => ToDo, { nullable: true })
-  todo(
-    @Arg("id", () => Int) id: number,
-    @Ctx() { em }: MyContext
-  ): Promise<ToDo | null> {
+  todo(@Arg("id") id: number, @Ctx() { em }: MyContext): Promise<ToDo | null> {
     return em.findOne(ToDo, { id });
+  }
+
+  @Mutation(() => ToDo)
+  async createToDo(
+    @Arg("description") description: string,
+    @Ctx() { em }: MyContext
+  ): Promise<ToDo> {
+    const todo = em.create(ToDo, { description });
+    await em.persistAndFlush(todo);
+    return todo;
   }
 }
