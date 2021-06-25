56565fd365cdddb2ae10c352e9dcce309dc722ab
Jennifer Kennedy <jenken131@gmail.com>
2021-06-24 19:20:33 -0700

    added todo query

--- server/src/resolvers/ToDo.ts
+++ server/src/resolvers/ToDo.ts
@@ -1,11 +1,19 @@
 import { ToDo } from "../entities/ToDo";
 import { MyContext } from "src/types";
-import { Query, Resolver, Ctx } from "type-graphql";
+import { Query, Resolver, Ctx, Arg, Int } from "type-graphql";
 
 @Resolver()
 export class ToDoResolver {
   @Query(() => [ToDo])
   todos(@Ctx() { em }: MyContext): Promise<ToDo[]> {
     return em.find(ToDo, {});
+  }
+
+  @Query(() => ToDo, { nullable: true })
+  todo(
+    @Arg("id", () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<ToDo | null> {
+    return em.findOne(ToDo, { id });
   }
 }
