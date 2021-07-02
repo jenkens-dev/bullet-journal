2325abd45210278707cde175dd17454b19159940
Jennifer Kennedy <jenken131@gmail.com>
2021-07-01 18:08:10 -0700

    updated folder name and added urql

---
+++ app/src/pages/register.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Formik, Form } from "formik";
+import { Button } from "@chakra-ui/react";
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
+
+interface registerProps {}
+
+const Register: React.FC<registerProps> = ({}) => {
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              placeholder="username"
+              label="Username"
+            />
+            <InputField
+              name="password"
+              placeholder="password"
+              label="Password"
+              type="password"
+            />
+            <Button
+              type="submit"
+              colorScheme="teal"
+              mt={4}
+              isLoading={isSubmitting}
+            >
+              Register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Register;
