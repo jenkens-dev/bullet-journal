ed23da28ac440fdb513c885fc75405e62bc21546
Jennifer Kennedy <jenken131@gmail.com>
2021-07-04 11:52:18 -0700

    added dynamic navbar depending on user login

--- app/src/components/NavBar.tsx
+++ app/src/components/NavBar.tsx
@@ -1,13 +1,21 @@
-import { Flex, Link, Box } from "@chakra-ui/react";
+import { Flex, Link, Box, Button } from "@chakra-ui/react";
 import React from "react";
 import NextLink from "next/link";
+import { useMeQuery } from "../generated/graphql";
 
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
-  return (
-    <Flex bg="tomato" p={4}>
-      <Box ml={"auto"}>
+  const [{ data, fetching }] = useMeQuery();
+  let body = null;
+
+  // data is loading
+  if (fetching) {
+    body = null;
+  } else if (!data?.me) {
+    // user not logged in
+    body = (
+      <>
         <NextLink href="/login">
           <Link color="white" mr={2}>
             Login
@@ -16,7 +24,20 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
         <NextLink href="/register">
           <Link color="white">Register</Link>
         </NextLink>
-      </Box>
+      </>
+    );
+  } else {
+    body = (
+      <Flex>
+        <Box mr={2}>{data.me.username}</Box>
+        <Button variant="link">Logout</Button>
+      </Flex>
+    );
+  }
+
+  return (
+    <Flex bg="tomato" p={4}>
+      <Box ml={"auto"}>{body}</Box>
     </Flex>
   );
 };
