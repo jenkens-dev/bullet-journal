5b0d08bb73203b975bceb0deab25f3ee525a1256
Jennifer Kennedy <jenken131@gmail.com>
2021-07-04 11:57:25 -0700

    install urql cache

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
-import { Provider, createClient } from "urql";
+import { Provider, createClient, dedupExchange, fetchExchange } from "urql";
+import { cacheExchange } from "@urql/exchange-graphcache";
 import theme from "../theme";
 
 const client = createClient({
   url: "http://localhost:4000/graphql",
+  exchanges: [dedupExchange, cacheExchange({}), fetchExchange],
   fetchOptions: {
     credentials: "include",
   },
