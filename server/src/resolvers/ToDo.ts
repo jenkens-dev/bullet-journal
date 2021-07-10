f8c786902acb581527ae14ac14926938fb5e7998
Jennifer Kennedy <jenken131@gmail.com>
2021-07-10 16:46:10 -0700

    converted todo resolver to use typeorm

--- server/src/resolvers/ToDo.ts
+++ server/src/resolvers/ToDo.ts
@@ -1,51 +1,39 @@
 import { ToDo } from "../entities/ToDo";
-import { MyContext } from "src/types";
-import { Query, Resolver, Ctx, Arg, Mutation } from "type-graphql";
+import { Query, Resolver, Arg, Mutation } from "type-graphql";
 
 @Resolver()
 export class ToDoResolver {
   @Query(() => [ToDo])
-  todos(@Ctx() { em }: MyContext): Promise<ToDo[]> {
-    return em.find(ToDo, {});
+  todos(): Promise<ToDo[]> {
+    return ToDo.find();
   }
 
-  @Query(() => ToDo, { nullable: true })
-  todo(@Arg("id") id: number, @Ctx() { em }: MyContext): Promise<ToDo | null> {
-    return em.findOne(ToDo, { id });
+  @Query(() => ToDo)
+  todo(@Arg("id") id: number): Promise<ToDo | undefined> {
+    return ToDo.findOne(id);
   }
 
   @Mutation(() => ToDo)
-  async createToDo(
-    @Arg("description") description: string,
-    @Ctx() { em }: MyContext
-  ): Promise<ToDo> {
-    const todo = em.create(ToDo, { description });
-    await em.persistAndFlush(todo);
-    return todo;
+  async createToDo(@Arg("description") description: string): Promise<ToDo> {
+    return ToDo.create({ description }).save();
   }
 
-  // TASK change to update and persist in one query
   @Mutation(() => ToDo, { nullable: true })
   async updateToDo(
     @Arg("id") id: number,
-    @Arg("description") description: string,
-    @Ctx() { em }: MyContext
+    @Arg("description") description: string
   ): Promise<ToDo | null> {
-    const todo = await em.findOne(ToDo, { id });
+    const todo = await ToDo.findOne(id);
     if (!todo) {
       return null;
     }
-    todo.description = description;
-    await em.persistAndFlush(todo);
+    await ToDo.update({ id }, { description });
     return todo;
   }
 
   @Mutation(() => Boolean)
-  async deleteToDo(
-    @Arg("id") id: number,
-    @Ctx() { em }: MyContext
-  ): Promise<boolean> {
-    await em.nativeDelete(ToDo, { id });
+  async deleteToDo(@Arg("id") id: number): Promise<boolean> {
+    await ToDo.delete(id);
     return true;
   }
 }
